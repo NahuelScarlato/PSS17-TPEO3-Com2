800094a10924bbc42852f26fb5423f134d98fead
FacundoMarin <facu1803@yahoo.com.ar>
2017-09-28 18:48:44 -0300

    asd

--- Proyecto/src/objeto/atravesable/Lava.java
+++ Proyecto/src/objeto/atravesable/Lava.java
@@ -1,9 +1,16 @@
 package objeto.atravesable;
 
+import objeto.noAtravesable.ObjetoNoAtravesable;
 import objeto.noAtravesable.objetoConVida.personaje.*;
 
 public class Lava extends ObjetoAtravesable{
 	public void modificar(Personaje p){
 		p.restarVida(5);
+	}
+
+	@Override
+	public void modificar(ObjetoNoAtravesable p) {
+		// TODO Auto-generated method stub
+		
 	}
 }
