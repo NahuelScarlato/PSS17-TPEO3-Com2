4bf10c59b53ccdfdafd10143afac0b42caea10b6
JuanmaFloyd <juanma_giron@hotmail.es>
2017-10-24 11:08:37 -0300

    audio

--- Proyecto/src/game/Launcher.java
+++ Proyecto/src/game/Launcher.java
@@ -1,9 +1,18 @@
 package game;
 
+import java.io.*;
+import sun.audio.*;
+
 public class Launcher {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
+		String audio = "C:/Users/Usuario/Documents/GitHub/Orc-Invasion-TD/Proyecto/res/Sounds/nix.wav";
+		InputStream in = new FileInputStream(audio);
+		AudioStream music = new AudioStream(in);
 		Game game = new Game("Juegazo", 768 , 384);
+		
 		game.start();
+		
+		AudioPlayer.player.start(music);
 	}
 }
