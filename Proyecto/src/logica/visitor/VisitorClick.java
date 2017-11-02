f10d8ab93ba5061e2486ebc807e3c773c0e829bb
JuanmaFloyd <juanma_giron@hotmail.es>
2017-11-02 10:18:32 -0300

    chau restar vida con click

--- Proyecto/src/logica/visitor/VisitorClick.java
+++ Proyecto/src/logica/visitor/VisitorClick.java
@@ -1,6 +1,5 @@
 package logica.visitor;
 
-import game.display.PremioManager;
 import objeto.noAtravesable.objetoConVida.OMConVida.OMConVida;
 import objeto.noAtravesable.objetoConVida.OTConVida.OTConVida;
 import objeto.noAtravesable.objetoConVida.Premios.Premio;
@@ -11,16 +10,13 @@ public class VisitorClick extends Visitor{
 	
 	//metodos
 	public void afectar(Aliado a) {
-		a.restarVida(1000);
 	}
 	public void afectar(Enemigo e) {
-		e.restarVida(1000);
+		System.out.println(e.getVida());
 	}
 	public void afectar(OTConVida otcv) {
-		otcv.restarVida(1000);
 	}
 	public void afectar(OMConVida omcv) {
-		omcv.restarVida(1000);
 	}
 	public void afectar(Premio p) {
 		p.serClickeado();
